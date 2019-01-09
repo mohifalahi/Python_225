86d451510be6cc23110d298c66c900e2a1d32382
Mohammad Falahi <45416720+mohifalahi@users.noreply.github.com>
2019-01-09 17:48:32 +0330

    making it easier
    based on the zen of python: simple is better.

--- two_fer.py
+++ two_fer.py
@@ -1,8 +1,4 @@
 def two_fer(name="you"):
-    user_name = input("Enter a name or leave it empty: ")
-    if not user_name:
-        return "One for {}, one for me.".format(name)
-    else:
-        return "One for {}, one for me.".format(user_name)
+    return "One for {}, one for me.".format(name)
 
 print(two_fer())
